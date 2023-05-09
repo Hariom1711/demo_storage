57551f44e59b497825a59567328c96a380b5990f
Hariom Ojha <2003om1711@gmail.com>
2023-05-09 11:58:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { db } from './db';
 
 function App() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const file = e.target.elements.file.files[0];
+    const name = e.target.elements.name.value;
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = async (e) => {
+      const dataUrl = e.target.result;
+      const icon = { name, dataUrl };
+      await db.icons.put(icon);
+      alert('Image saved to IndexedDB!');
+    };
+  };
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const name = e.target.elements.name.value;
+    const icon = await db.icons.get(name);
+    if (icon) {
+      const img = document.getElementById('img');
+      img.src = icon.dataUrl;
+      alert('Image found in IndexedDB!');
+    } else {
+      alert('Image not found in IndexedDB!');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IndexedDB Image Storage</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Icon Name:</label>
+        <input type="text" id="name" name="name" />
+        <br />
+        <label htmlFor="file">Icon Image:</label>
+        <input type="file" id="file" name="file" />
+        <br />
+        <button type="submit">Save Image</button>
+      </form>
+      <br />
+      <form onSubmit={handleSearch}>
+        <label htmlFor="name">Search Icon:</label>
+        <input type="text" id="name" name="name" />
+        <button type="submit">Search</button>
+      </form>
+      <br />
+      <img id="img" src="" alt="" width={300} height={300}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+        console.log('Service worker registered successfully:', registration);
+      })
+      .catch(function(error) {
+        console.log('Error registering service worker:', error);
+      });
+  });
+}
+
 reportWebVitals();
